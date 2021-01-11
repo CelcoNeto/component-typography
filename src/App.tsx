b3ef8294b1c7aa5009d64994226ddb72674ad27e
Celço Neto <celcoroxa@gmail.com>
2021-01-11 20:55:36 -0300

    feat: add storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { Typography } from './shared/components/typography';
-import { theme } from './theme/globals';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { Typography } from "./shared/components/typography";
+import { theme } from "./theme/globals";
 
 export const App: React.FC = () => (
   <ThemeProvider theme={theme}>
-    <Typography size='large' tag='subtitle2' fontColor='magenta'>
+    <Typography size="large" tag="h1" fontColor="magenta">
       Teste
     </Typography>
   </ThemeProvider>
